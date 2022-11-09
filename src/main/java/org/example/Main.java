170fb7fc856778a0917589f60eb42064dc6d9115
daya <dayajuangmahaputra@gmail.com>
2022-11-09 10:41:00 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,27 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+    private static final int MIN_TO_HOUR = 60;
+    private static final int HOUR_TO_DAY = 24;
+    private static final int DAY_TO_YEAR = 365;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printYearsAndDays(525600);
+    }
+
+    public static void printYearsAndDays(long minutes){
+        // Check if the input is valid
+        if(minutes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        // Calculate the year and days
+        int days = (int) (minutes / MIN_TO_HOUR) / HOUR_TO_DAY;
+        int year = days / DAY_TO_YEAR;
+
+        // Check the remaining days
+        days %= DAY_TO_YEAR;
+
+        // Print the result
+        System.out.println(minutes + " min = " + year +" y and " + days + " d");
     }
 }
